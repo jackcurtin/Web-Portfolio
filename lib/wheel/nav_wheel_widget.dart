77f248aea3122bac4b38fc3efe9ba06b54a99543
Jack Curtin <jackcurtin94@gmail.com>
2024-06-13 13:07:16 -0500

    removed unused stuff

--- lib/wheel/nav_wheel_widget.dart
+++ lib/wheel/nav_wheel_widget.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/wheel/circle_painter.dart';
+import 'package:circular_menu/circular_menu.dart';
 
 class NavWheel extends StatelessWidget {
   const NavWheel({super.key});
 
   Widget _buildCircle(BuildContext context) {
-    double _height = MediaQuery.sizeOf(context).height / 2;
-    return CustomPaint(
-      size: Size(_height, _height),
-      painter: CirclePainter(),
-    );
+    final circularMenu = CircularMenu(items: [
+      CircularMenuItem(
+          color: Colors.blue,
+          iconColor: Colors.white,
+          iconSize: 30.0,
+          margin: 10.0,
+          padding: 10.0,
+          icon: Icons.home,
+          onTap: () {
+            // callback
+          }),
+      CircularMenuItem(
+          icon: Icons.search,
+          onTap: () {
+            //callback
+          }),
+      CircularMenuItem(
+          icon: Icons.settings,
+          onTap: () {
+            //callback
+          }),
+      CircularMenuItem(
+          icon: Icons.star,
+          onTap: () {
+            //callback
+          }),
+      CircularMenuItem(
+          icon: Icons.pages,
+          onTap: () {
+            //callback
+          }),
+    ]);
+    return circularMenu;
   }
 
   @override
